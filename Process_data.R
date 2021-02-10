0e60847630c1a9eb31acf018f13a5ea00d4c5a44
lilfijj <jacqueline.r.griffin@dartmouth.edu>
2021-02-10 13:04:00 -0500

    Anneka p/u

--- Process_data.R
+++ Process_data.R
@@ -1,67 +1,45 @@
-## load packages ----
-
-
-install.packages(c("lattice", "readxl","VIM","ggplot2","tidyr","mice","tidyr","dplyr"))
+# libraries ----
 library(mice)
 library(lattice)
 library(readxl)  
 library(VIM)
 library(ggplot2)
 library(tidyr)
-library(mice)
 library(dplyr)
-library(VIM)
+
+# load ----
+  setwd ("C:/Users/jackie/OneDrive - Dartmouth College/Mice_for_data_imputing/") 
+  metabolomics <- read.csv("082020RajohnsonMet1_9_.csv", stringsAsFactors = FALSE, header = TRUE, row.names = 1)
+
+# clean ----
+  colnames(metabolomics) <- c('Baseline_1','Baseline_2','Baseline_3','Baseline_4', 'Baseline_5', 
+                              'Dormant_1', 'Dormant_2', 'Dormant_3', 'Dormant_4')
+  meta <- metabolomics[-c(1,2),-c(10:12)] # remove unwanted rows, cols
+  head(meta)
+  
+  meta_clean <- as.data.frame(apply(meta,2, as.numeric)) # change str to num 
+  str(meta_clean)
+  
+  meta_names <- row.names(meta) # create vector with meta row.names
+  row.names(meta_clean) <- meta_names # re-introduce to meta_clean
+  head(meta_clean)
+  
+  meta_clean_t <- as.data.frame(t(meta_clean)) # transpose 
+  head(meta_clean_t)
+  str(meta_clean_t)
+  class(meta_clean_t)
+
+# Missing data ----
+  pMiss <- function(meta_clean_t){sum(is.na(meta_clean_t))/length(meta_clean_t)*100} # % missing data per sample/rows
+  apply(meta_clean_t,1,pMiss) # consider removing samples with >30% missing data
+  meta_clean_t_filt <- meta_clean_t[ , colSums(is.na(meta_clean_t)) < nrow(meta_clean_t)] # remove metabolites/cols with all NAs 
+  aggr_plot <- aggr(meta_clean_t_filt, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, # visualize missing data
+                    labels=names(meta_clean_t), cex.axis=.7, gap=3, 
+                    ylab=c("Histogram of missing data","Pattern"))
+  
+  meta_clean_t_filt_IMP <- mice(meta_clean_t_filt, maxit = 10, print=F) # impute missing data using MICE
+  colnames(meta_clean_t_filt) <- gsub(" ", "_", colnames(meta_clean_t_filt))
+
+# column/metabolite names need cleaned up... 
 
 
-## data processing ----
-
-# set working directory
-setwd ("C:/Users/jackie/OneDrive - Dartmouth College/Mice_for_data_imputing/") 
-
-#Load data into R and name it metabolomics
-metabolomics <- read.csv("082020RajohnsonMet1_9_.csv", stringsAsFactors = FALSE, header = TRUE, row.names = 1)
-
-# rename columns to match treatment replicates
-colnames(metabolomics) <- c('Baseline_1','Baseline_2','Baseline_3','Baseline_4', 'Baseline_5',
-                            'Dormant_1', 'Dormant_2', 'Dormant_3', 'Dormant_4')
-head(metabolomics)
-meta <- metabolomics[-c(1,2), ] # remove rows 1 and 2 
-meta <- meta[,-c(10:12) ] # remove columns 10 through 12
-head(meta)
-
-# clean data
-meta_clean <- as.data.frame(apply(meta,2, as.numeric)) # change all cols from chr to num
-str(meta_clean)
-head(meta_clean)
-
-meta_names <- row.names(meta) # make a vector with row names from meta df
-row.names(meta_clean) <- meta_names # re-introduce row names to clean df
-head(meta_clean)
-
-# transpose - samples are row ids, features as column ids
-meta_clean_t <- as.data.frame(t(meta_clean))
-class(meta_clean_t)
-str(meta_clean_t)
-
-# Check for samples (rows) with > 30% missing data, if any, consider removing from analysis
-pMiss <- function(meta_clean_t){sum(is.na(meta_clean_t))/length(meta_clean_t)*100}
-apply(meta_clean_t,1,pMiss)
-
-# visualize missing data
-aggr_plot <- aggr(meta_clean_t, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, labels=names(meta_clean_t), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
-
-# remove features (columns) with all NAs 
-meta_clean_t_filt <- meta_clean_t[ , colSums(is.na(meta_clean_t)) < nrow(meta_clean_t)]
-
-# re-plot
-aggr_plot <- aggr(meta_clean_t_filt, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, labels=names(meta_clean_t), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
-
-## MICE ----
-imp <- mice(meta_clean_t_filt, maxit = 10, print=F)
-
-## Need to remove spaces from column names
-
-colnames(meta_clean_t_filt) <- gsub(" ", "_", colnames(meta_clean_t_filt))
-
-
-## column names need cleaned up..
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# metabolomics_b
+# Metabolomics data from estrogen receptor+ breast tumors at two different disease stages:
+  - Primary control, proliferative growth phase
+  - 90 days dormant, minimal residual disease 
+  
+# Objective: Clean-up raw data frame and impute missing data using MICE package.
